43cdbd8bc330375dbec84cd33d4dc14d106618cf
Mike Aguilar <miguelaguilar79@gmail.com>
2021-07-26 16:54:47 -0700

    Made changes to the IPTables config and also modified some other minor configurations in hostapd conf files.

--- WiFiAP/appconfig/iptables.sh
+++ WiFiAP/appconfig/iptables.sh
@@ -1,7 +1,10 @@
 #/bin/sh
-iptables-nft -t nat -C POSTROUTING -o eth0 -j MASQUERADE
-iptables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables-nft -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables-nft -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables-nft -C FORWARD -i wlan0 -o eth0 -j ACCEPT
-iptables-nft -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables-nft -t nat -C POSTROUTING -o eth0 -j MASQUERADE || iptables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables-nft -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT || iptables-nft -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables-nft -C FORWARD -i wlan0 -o eth0 -j ACCEPT || iptables-nft -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+
+# Other example below
+#iptables -t nat -A POSTROUTING -o usbnet0 -j MASQUERADE
+#iptables -A FORWARD -i eth0 -o usbnet0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -i usbnet0 -o eth0 -j ACCEPT
